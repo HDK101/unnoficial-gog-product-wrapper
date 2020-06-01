94294064807410540e51d0e3e10440e7b6d9e55c
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2020-06-01 18:38:04 -0300

    Added gitignore and updated folder name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { getProductById } = require("./src/gogwrapper")
+const { getProductById } = require("./src/gogwrapper.test")
 
 getProductById("1207658787").then(function(product) {
     console.log(product.getTitle())
