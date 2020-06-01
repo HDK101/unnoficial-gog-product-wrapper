c4d47afae2453072b7619acfca37850a32707b81
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2020-06-01 18:42:54 -0300

    Fixed import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { getProductById } = require("./src/gogwrapper.test")
+const { getProductById } = require("./src/gogwrapper")
 
 getProductById("1207658787").then(function(product) {
     console.log(product.getTitle())
