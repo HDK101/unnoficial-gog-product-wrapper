84380cc102ece102fbbfd3b815eefa95078dc985
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2020-06-01 18:33:58 -0300

    Files

---
+++ index.js
@@ -0,0 +1,14 @@
+const { getProductById } = require("./src/gogwrapper")
+
+getProductById("1207658787").then(function(product) {
+    console.log(product.getTitle())
+    console.log(product.getReleaseDate())
+    console.log(product.getAvailableLanguages())
+    console.log(product.getAvailableSystems())
+    console.log(product.getImages(true))
+    product.getPrices("BR").then(function(basePrice) {
+        console.log(basePrice)
+    })
+}).catch(function(err) {
+    console.log(err)
+})
