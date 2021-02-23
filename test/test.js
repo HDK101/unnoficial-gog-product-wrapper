e1176dae454a45302dcb06423beca80391daa6b1
Lucas Hideki <lucashideki87@gmail.com>
2021-02-23 11:24:24 -0300

    Written tests

---
+++ test/test.js
@@ -0,0 +1,64 @@
+const assert = require("assert");
+const { getProductById, gogProduct } = require("../src/gogwrapper");
+
+describe("Getting a JSON of a GOG product by it ID and getting a class", function () {
+    this.timeout(10000);
+    describe("#getProductById()", function () {
+        it("should return a resolved promise", function () {
+            return getProductById("1441039322");
+        });
+    });
+});
+
+describe("Functions of GOG Product", function () {
+    var currentProduct = null
+    before("Get GOG Product", function (done) {
+        getProductById("1441039322").then(function (product) {
+            currentProduct = product;
+            done();
+        });
+    });
+
+    it("#getTitle", function () {
+        var title = currentProduct.getTitle();
+        console.log("Title: ", title);
+        assert(title);
+    });
+    it("#getProductCard", function () {
+        var productCard = currentProduct.getProductCard();
+        console.log("Product Card: ", productCard);
+        assert(productCard);
+    });
+    it("#getPurchaseLink", function () {
+        var purchaseLink = currentProduct.getPurchaseLink();
+        console.log("Purchase link: ", purchaseLink);
+        assert(purchaseLink);
+    });
+    it("#getReleaseDate", function () {
+        var releaseDate = currentProduct.getReleaseDate();
+        console.log("Release Date: ", releaseDate);
+        assert(releaseDate);
+    });
+    it("#getAvailableLanguages", function () {
+        var availableLanguages = currentProduct.getAvailableLanguages();
+        console.log("Available Languages: ", availableLanguages);
+        assert(availableLanguages);
+    });
+    it("#getImages", function () {
+        const { background, logo, icon } = currentProduct.getImages();
+        console.log("Background: ", background, "\nLogo: ", logo, "\nIcon:, icon");
+        assert(background);
+        assert(logo);
+        assert(icon);
+    });
+    it("#getPrices, with 'US as parameter'", function () {
+        return currentProduct.getPrices("US").then(function (prices) {
+            console.table(prices);
+        });
+    });
+    it("#getTotalRatingsValue", function () {
+        return currentProduct.getTotalRatingsValue().then(function (ratingsValue) {
+            console.log("Rating: ", ratingsValue);
+        });
+    });
+});
