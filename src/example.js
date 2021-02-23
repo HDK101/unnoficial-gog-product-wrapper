b5905b03c2059838d7024cc12347c92c115de700
Lucas Hideki <lucashideki87@gmail.com>
2021-02-23 11:31:30 -0300

    example

---
+++ src/example.js
@@ -0,0 +1,18 @@
+const { getProductById } = require("./src/gogwrapper")
+
+/* getProductById("1207658787").then(function(product) { */
+getProductById("1441039322").then(function (product) {
+    console.log(product.getTitle())
+    console.log(product.getReleaseDate())
+    console.log(product.getAvailableLanguages())
+    console.log(product.getAvailableSystems())
+    console.log(product.getImages(true))
+    product.getPrices("US").then(function (priceJson) {
+        console.table(priceJson);
+    });
+    product.getTotalRatingsValue().then(function (ratingsValue) {
+        console.log("Rating: ", ratingsValue);
+    });
+}).catch(function (err) {
+    console.log(err);
+})
